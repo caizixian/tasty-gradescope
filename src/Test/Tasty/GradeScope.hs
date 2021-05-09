62753a52ac499eb61b6ff30a91dcf0bec7d31752
Zixian Cai <u5937495@anu.edu.au>
2021-05-09 21:53:41 +1000

    Get rid of test id

--- src/Test/Tasty/GradeScope.hs
+++ src/Test/Tasty/GradeScope.hs
@@ -158,7 +158,6 @@ instance JSON TestResult where
       [ ("name"       , showJSON resultName)
       , ("score"      , showJSON score)
       , ("max_score"  , showJSON resultWeight)
-      , ("number"     , showJSON resultId)
       , ("output"     , showJSON (Runners.resultDescription resultMetadata))
       , ("visibility" , showJSON resultVisible)
       ]
